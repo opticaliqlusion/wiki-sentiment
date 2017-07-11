7a16d669beb3890b671fe3c9ea53b88ed7783106
opticaliqlusion <john.laky1@gmail.com>
2017-07-10 20:43:54 -0400

    added annotations

--- make_graph.py
+++ make_graph.py
@@ -1,26 +1,39 @@
 import plotly
 import plotly.graph_objs as go
 
-import random
-import csv
-
+plotly.offline.init_notebook_mode()
 
 def make_graph(set_dict):
 
     data = []
+    frames = {}
+
     scale = 3
 
+    mk_frame = lambda name,x,y,z : dict(x=x,
+                        y=y,
+                        mode='markers',
+                        name=k,
+                        marker=dict(size=z*scale))
+
     for k in set_dict.keys():
-        
+
         ele = set_dict[k]
-    
+
+        x = ele[0]
+        y = ele[1]
+        z = ele[2]
+
+        lengths = ele[3]
+
         trace = go.Scatter(
-            x = ele[0],
-            y = ele[1],
+            x = x,
+            y = y,
             name = k,
+            text = ['Magnitude:{}'.format(z[i]) for i in range(len(x))],
             mode = 'markers',
             marker = dict(
-                size = [i * scale for i in ele[2]],
+                size = [i * scale for i in z],
                 #color = 'rgba(152, 0, 0, .8)',
                 line = dict(
                     width = 1,
@@ -31,10 +44,51 @@ def make_graph(set_dict):
 
         data += [trace]
 
+        for i in range(len(x)):
+            if i not in frames:
+                frames[i] = []
+            frames[i] += [mk_frame(k, x[i],y[i],z[i])]
+
+    frames = [{'data':i} for i in frames.values()]
+
+
     layout = dict(title = 'Wikipedia Sentiment Analysis',
                   yaxis = dict(zeroline = False),
-                  xaxis = dict(zeroline = False)
-                 )
+                  xaxis = dict(zeroline = False),
+                  annotations=[
+                    dict(
+                        x='2005-06-01',
+                        y=0.5,
+                        xref='x',
+                        yref='y',
+                        text='Positive sentiment, small confidence',
+                        showarrow=True,
+                        arrowhead=0,
+                        ax=0,
+                        ay=-40),
+                    dict(
+                        x='2017-07-01',
+                        y=-0.1,
+                        xref='x',
+                        yref='y',
+                        text='Negative sentiment, large confidence',
+                        showarrow=True,
+                        arrowhead=0,
+                        ax=0,
+                        ay=-40),
+                    dict(
+                        x='2012-01-01',
+                        y=0.6,
+                        xref='x',
+                        yref='y',
+                        text='Applying Google Sentiment Analysis to evolving wikipedia pages.',
+                        showarrow=False,
+                        arrowhead=0,
+                        ax=0,
+                        ay=-40)
+                    ]
+                )
+
 
     fig = dict(data=data, layout=layout)
     plotly.offline.plot(fig, filename='styled-scatter')
